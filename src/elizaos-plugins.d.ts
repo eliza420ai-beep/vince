84fec108eeae312b6dfee914ae0910c98ac797b2
IKIGAI LABS XYZ <ikigailabs@proton.me>
2026-02-02 20:35:57 +0100

    VINCE

---
+++ src/elizaos-plugins.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Type declarations for ElizaOS plugins when TS cannot resolve their built-in types.
+ */
+declare module "@elizaos/plugin-sql" {
+  const plugin: unknown;
+  export default plugin;
+}
+
+declare module "@elizaos/plugin-bootstrap" {
+  const plugin: unknown;
+  export default plugin;
+}
